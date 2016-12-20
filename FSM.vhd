a70bf3bfe00a5cfb95deb25d3de74f1302a8e58a
channtron <p.guti.jime@gmail.com>
2016-12-20 16:46:11 +0100

    Update FSM.vhd

--- FSM.vhd
+++ FSM.vhd
@@ -11,17 +11,40 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity FSM is
-    Port ( clk : in  STD_LOGIC;
-           reset : in  STD_LOGIC;
-           control : in  STD_LOGIC_VECTOR (1 downto 0);
-           direc : out  STD_LOGIC_VECTOR (7 downto 0);
-           data : in  STD_LOGIC_VECTOR (7 downto 0));
+    Port ( mov : in  STD_LOGIC_VECTOR (1 downto 0); --movimiento procedente del teclado
+--           FSM_Plotter : out  STD_LOGIC_VECTOR (1 downto 0); --información que se enviará al plotter y a la musica
+           bdir : out  STD_LOGIC_VECTOR (7 downto 0);
+           bdata : in  STD_LOGIC_VECTOR (4 downto 0));
 end FSM;
 
 architecture Behavioral of FSM is
-
+	type mi_estado is (Inicio, Reposo, Movimiento,  Analisis, KO, Avanza, Sumar, OK); --estados
+	signal estado,p_estado: mi_estado;
+	signal Dserp,p_Dserp,Dcola,p_Dcola : std_logic_vector(7 downto 0); --refistros de direcciones
+	signal p_casilla : std_logic_vector (3 downto 0); --registro para analizar las casillas
+	signal RS :std_logic_vector (4 downto 0); --bms bit de inicio, 3 y 2 mov cola, 1 y 0 mov cabeza
 begin
-
+	sync: process (clk, reset)
+		begin 
+			if (reset='1')then
+				estado<=Inicio;
+			elsif (rising_edge(clk) and reset='0') then
+				estado<=p_estado;
+			end if;
+		end process;
+	
+	comb: process(estado)
+		begin
+			case estado is
+				when inicio =>
+				when reposo=>
+				when movimiento=>
+				when analisis=>
+				when avanza=>
+				when sumar=>
+				when OK=>
+				when KO=>
+			end case;
+		end process;
 
 end Behavioral;
-
