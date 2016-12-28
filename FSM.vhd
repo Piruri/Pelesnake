01ae4f835d9eb0941b19d8e3020ca567af28835c
channtron <p.guti.jime@gmail.com>
2016-12-28 18:48:43 +0100

    Update FSM.vhd

--- FSM.vhd
+++ FSM.vhd
@@ -4,7 +4,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity FSM is
-	Port (tframe : in std_logic; --señal vsinc del vga, está a 0 un clk al refrescar terminar una pantalla    
+    Generic( CNT: integer :=26);
+	Port (
+     reset : in std_logic;
+     clk : in std_logic;
+     tframe : in std_logic; --señal vsinc del vga, está a 0 un clk al refrescar terminar una pantalla    
 	mov : in  STD_LOGIC_VECTOR (1 downto 0); --movimiento procedente del teclado
 --  FSM_Plotter : out  STD_LOGIC_VECTOR (1 downto 0); --información que se enviará al plotter y a la musica
 	bdir : out  STD_LOGIC_VECTOR (7 downto 0); --bus direcciones
@@ -15,7 +19,7 @@ end FSM;
 architecture Behavioral of FSM is
    type mi_estado is (Inicio, Reposo, Movimiento,  Analisis, KO, Avanza, Sumar, OK); --estados
    signal estado,p_estado: mi_estado;
-   signal cuenta, pcuenta: unsigned; --contador
+   signal cuenta, p_cuenta: unsigned; --contador
    signal flag: std_logic; --para evitar que cuente de más
    signal Dserp,p_Dserp,Dcola,p_Dcola : unsigned(7 downto 0); --registros de direcciones
    signal p_casilla : std_logic_vector (3 downto 0); --registro para analizar las casillas
@@ -50,9 +54,9 @@ begin
    comb: process(estado)
    	begin
        	case estado is
-           	when inicio =>
+   				 when inicio =>
              	RS(4)<='1'; --bit de inicio
-         	when reposo=>
+   				 when reposo=>
                	if(RS(4)='1') then --se viene de inicio
                 	RS(1 downto 0)<=mov;
                  	RS(4)<='0';
@@ -61,75 +65,75 @@ begin
                 	RS(4)<='0';
                 	p_estado<=movimiento;
            	end if;
-           	when Movimiento=>
+   				 when Movimiento=>
              	case RS(1 downto 0) is --se ve el ultimo movimiento
                 	when "00" => --arriba
-                     	if (mov|="11") then --si no se esta realizando el mov contrario se guarda
+                     	if (mov/="11") then --si no se esta realizando el mov contrario se guarda
                         	RS(1 downto 0)<=mov;
                      	else
                         	RS(1 downto 0)<="00"; --si no se mantiene
                      	end if;
                     	if (cuenta = CNT) then --si la cuenta llega al final se avanza
                           	p_estado <= analisis;
-                         	p_cuenta=0;
+                         	p_cuenta<=(others=>0);
                       	else
                           	p_estado <= estado;
                          	p_cuenta<=cuenta;
                      	end if;
                  	when "01" => --derecha
-                     	if (mov|="10") then
+                     	if (mov/="10") then
                         	RS(1 downto 0)<=mov;
                      	else
                         	RS(1 downto 0)<="01";
                      	end if;
                     	if (cuenta = CNT) then
                           	p_estado <= analisis;
-                         	p_cuenta=0;
+                         	p_cuenta<=(others=>0);
                       	else
                           	p_estado <= estado;
                          	p_cuenta<=cuenta;
                      	end if;   	 
                  	when "10" => --izquierda
-                     	if (mov|="01") then
+                     	if (mov/="01") then
                         	RS(1 downto 0)<=mov;
                      	else
                         	RS(1 downto 0)<="10";
                      	end if;
                     	if (cuenta = CNT) then
                           	p_estado <= analisis;
-                         	p_cuenta=0;
+                         	p_cuenta<=(others=>0);
                       	else
                           	p_estado <= estado;
                          	p_cuenta<=cuenta;
                      	end if;
                  	when "11" => --abajo
-                    	if (mov|="00") then
+                    	if (mov/="00") then
                         	RS(1 downto 0)<=mov;
                      	else
                         	RS(1 downto 0)<="11";
                      	end if;
                     	if (cuenta = CNT) then
                           	p_estado <= analisis;
-                         	p_cuenta=0;
+                         	p_cuenta<=(others=>0);
                       	else
                           	p_estado <= estado;
                          	p_cuenta<=cuenta;
                      	end if;
                      	when others => --en otro caso(para evitar latch) se hace como si fuese hacia arriba
-                         	if (mov|="11") then
+                         	if (mov/="11") then
                         	RS(1 downto 0)<=mov;
                      	else
                         	RS(1 downto 0)<="00";
                      	end if;
                     	if (cuenta = CNT) then
                           	p_estado <= analisis;
-                         	p_cuenta=0;
+                         	p_cuenta<=(others=>0);
                       	else
                           	p_estado <= estado;
                          	p_cuenta<=cuenta;
                      	end if;
                	end case;
-           	when analisis=>
+   				 when analisis=>
             	case RS(1 downto 0) is --se genera la proxima direccion de la cabeza
                 	when "00" =>
                      	p_Dserp <= Dserp - 16; --se resta una linea vertical
@@ -142,14 +146,14 @@ begin
                     	when others =>
                          	p_Dserp <= Dserp;
               	end case;
-            	bdir<=p_Dserp; --se escribe la casilla
+            	bdir<=std_logic_vector(p_Dserp); --se escribe la casilla
             	p_casilla<=bdata;
    					 if(p_casilla(3)='1')then --si el bms es uno es muro o cola
                 	p_estado<=ko;
    					 else
                 	p_estado<=avanza; --si no avanza
    					 end if;
-           	when avanza=>
+   				 when avanza=>
    					 if(p_casilla(1)='1')then --si el bit 1 es 1 es una seta
                 	p_estado<=sumar;
    					 else
@@ -157,10 +161,10 @@ begin
    					  end if;
            	when sumar=>
             	rw<='1'; --se va a escribir en la memoria
-            	bdir<=p_Dserp; --se escribe la nueva cabeza
+            	bdir<=std_logic_vector(p_Dserp); --se escribe la nueva cabeza
             	bdata(3 downto 0)<="01";
                 	bdata(0 downto 1)<=mov;
-            	bdir<=Dserp; --se escribe en la antigua cabeza un cuerpo
+            	bdir<=std_logic_vector(Dserp); --se escribe en la antigua cabeza un cuerpo
             	bdata(3 downto 0)<="10";
                 	bdata(0 downto 1)<=mov;
             	p_estado<=reposo;           	 
