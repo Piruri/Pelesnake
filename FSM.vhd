2d8323ca81bf4abf33b9ec2cb7a654f70be707f8
channtron <p.guti.jime@gmail.com>
2017-01-17 14:47:51 +0100

    Update FSM.vhd

--- FSM.vhd
+++ FSM.vhd
@@ -31,14 +31,29 @@ signal mov,pmov :  STD_LOGIC_VECTOR (1 downto 0); --vector de movimiento
 signal direcciones : std_logic_vector (3 downto 0); --Para codificar el movimiento
 signal bdirs, pbdirs : std_logic_vector (7 downto 0); --Señales para hacer sincronas esa salida
 signal bdatins, pbdatins : std_logic_vector (3 downto 0); -- Señales para hacer sincrona la salida
+signal auxtframe,pflag,flag: std_logic;
 begin
 	
 direcciones(0)<=UP;
 direcciones(1)<=LEF;
 direcciones(2)<=RIG;
 direcciones(3)<=DOW;
-comb:process (direcciones,mov) --Codificacin para el movimiento
+comb:process (direcciones,mov,tframe, flag) --Codificacin para el movimiento
 	begin
+		if (tframe='0') then
+		pflag<='1';
+		else
+		pflag<='0';
+		end if;
+		if (flag='0' and tframe='0') then
+		auxtframe<='1';
+--		elsif (flag='1' and tframe='0') then
+--		auxtframe<='0';
+		else
+		auxtframe<='0';
+		end if;
+				
+		
 		case direcciones is
 			when "0001" => --
 				pmov <= "00";
@@ -58,7 +73,7 @@ comb:process (direcciones,mov) --Codificacin para el movimiento
 -----------------------------------------------------------
 -----------------------------------------------------------
 
-   estadosync: process (clk, reset, tframe) --Actualizacin de datos
+   estadosync: process (clk, reset) --Actualizacin de datos --tframe
        begin
            if (reset='1')then
                estado<=Inicio;
@@ -71,11 +86,12 @@ comb:process (direcciones,mov) --Codificacin para el movimiento
 					mov<="00";
 					bdirs <= (others => '0'); -- OJO, si reset=1, la dir de mem va a 0.
 					bdatins <= (others => '0'); -- OJO. Este realmente debe tener el valor que vaya en la dir "00..0"
-           elsif (rising_edge(clk) and reset='0') then
+					flag<='0';
+			  elsif (rising_edge(clk) and reset='0') then
                estado<=p_estado;
-					if(tframe='0') then
+--					if(tframe='0') then
 						cuenta<=p_cuenta;
-					end if;
+--					end if;
 					Dserp <= pDserp;
 					Dcola <= pDcola;
 					casilla <= pcasilla;
@@ -84,6 +100,7 @@ comb:process (direcciones,mov) --Codificacin para el movimiento
 					mov<=pmov;
 					bdirs <= pbdirs;
 					bdatins <= pbdatins;
+					flag<=pflag;
            end if;
        end process;
 -----------------------------------------------------------
@@ -92,7 +109,7 @@ bdir <= bdirs;
 bdatin <= bdatins;
 -----------------------------------------------------------
 
-   combi: process(estado,cuenta,mov,bdatout,Dserp,Dcola,direcciones,RS,nxDserp,casilla)
+   combi: process(estado,cuenta,mov,bdatout,Dserp,Dcola,direcciones,RS,nxDserp,casilla,bdirs,bdatins,auxtframe)
        begin
 				pDserp <= Dserp;
 			  pnxDserp <= nxDserp;
@@ -132,10 +149,11 @@ bdatin <= bdatins;
                  case RS(1 downto 0) is --se ve el ultimo movimiento
                     when "00" => --arriba
 						  pRS (4 downto 2) <= RS (4 downto 2);
-                         if (mov/="11" and cuenta = CNT) then --si no se esta realizando el mov contrario se guarda
+                         if (mov/="11" and auxtframe = '1') then --si no se esta realizando el mov contrario se guarda
                             pRS(1 downto 0)<=mov;
 									 p_estado <= CalculoCasilla;
                             p_cuenta<=(others=>'0');
+								
                          else
                             pRS(1 downto 0)<="00"; --si no se mantiene
 									 p_estado <= estado;
@@ -144,7 +162,7 @@ bdatin <= bdatins;
           
                      when "01" => --derecha
 							pRS (4 downto 2) <= RS (4 downto 2);
-                         if (mov/="10" and cuenta = CNT) then
+                         if (mov/="10" and auxtframe = '1') then
                             pRS(1 downto 0)<=mov;
 									 p_estado <= CalculoCasilla;
                             p_cuenta<=(others=>'0');
@@ -156,7 +174,7 @@ bdatin <= bdatins;
 								 
                      when "10" => --izquierda
 							pRS (4 downto 2) <= RS (4 downto 2);
-                         if (mov/="01"and cuenta = CNT) then
+                         if (mov/="01"and auxtframe = '1') then
                             pRS(1 downto 0)<=mov;
 									 p_estado <= CalculoCasilla;
                             p_cuenta<=(others=>'0');
@@ -168,7 +186,7 @@ bdatin <= bdatins;
 								 
                      when "11" => --abajo
 							pRS (4 downto 2) <= RS (4 downto 2);
-                        if (mov/="00"and cuenta = CNT) then
+                        if (mov/="00"and auxtframe = '1') then
                             pRS(1 downto 0)<=mov;
 									 p_estado <= CalculoCasilla;
                             p_cuenta<=(others=>'0');
@@ -180,7 +198,7 @@ bdatin <= bdatins;
 								 
                          when others => --en otro caso(para evitar latch) se hace como si fuese hacia arriba
 								 pRS (4 downto 2) <= RS (4 downto 2);
-								if (mov/="11"and cuenta = CNT) then
+								if (mov/="11"and auxtframe = '1') then
                             pRS(1 downto 0)<=mov;
 									 p_estado <= CalculoCasilla;
                             p_cuenta<=(others=>'0');
@@ -212,7 +230,7 @@ bdatin <= bdatins;
 					when analisis=>
 						pbdirs<=std_logic_vector(nxDserp); --se escribe la casilla
 						pcasilla<=bdatout;
-						if (cuenta=2) then
+						if (cuenta=6) then
 								p_cuenta<=(others=>'0');
 								if(casilla(3)='1')then --si el bMs es uno es muro o cola
 								  p_estado<=ko;
@@ -236,7 +254,7 @@ bdatin <= bdatins;
                when sumar=>
 						pRS <= RS;
 						rw<="1"; --se va a escribir en la memoria
-						if (cuenta=2) then 
+						if (cuenta=6) then 
 							pbdirs<=std_logic_vector(Dserp); --se escribe en la antigua cabeza un cuerpo
 							pbdatins(3 downto 2)<="10";
 							pbdatins(1 downto 0)<=mov;
@@ -292,4 +310,4 @@ bdatin <= bdatins;
            end case;
        end process;
 		 
-end Behavioral;;
+end Behavioral;
